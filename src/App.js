9396ed21531d1ec77bb57b4c92624e6831696c29
RemyVila <josephvila123@gmail.com>
2020-11-06 11:24:48 -0500

    dependencies, axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import styled, { keyframes } from "styled-components";
+
 
 const App = () => {
+
+  const [RMData, SetRMData] = useState({});
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/291')
+      .then(res => {
+        console.log('this is the res', res.data);
+      })
+      .catch(err => {
+        console.log('this is the err', err);
+      })
+  } ,[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +25,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick & Morty Characters</h1>
     </div>
   );
 }
